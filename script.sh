29b7032e2c0a79b1dd45022349b22861fb27d055
Alfonso Carrasco <ing.alfonso.carrasco@gmail.com>
2023-12-06 06:19:22 -0600

    feature(functions): initial script to create a clone db

--- script.sh
+++ script.sh
@@ -8,12 +8,21 @@ else
     exit 1
 fi
 
+DATE_FORMAT=$(date +"%Y%m%d_%H%M%S")
+
 # Obtén las credenciales de KMS
-DB_USER=$(aws kms decrypt --key-id $KMS_KEY_ID --ciphertext-blob fileb://<(echo "$ENCRYPTED_DB_USER" | base64 -d) --output text --query Plaintext | base64 -d)
-DB_PASSWORD=$(aws kms decrypt --key-id $KMS_KEY_ID --ciphertext-blob fileb://<(echo "$ENCRYPTED_DB_PASSWORD" | base64 -d) --output text --query Plaintext | base64 -d)
-DB_INSTANCE=$("$ACALSA_DB_INSTANCE" | base64 -d)
-DB_NAME=$("$ACALSA_DB_NAME" | base64 -d)
-BACKUP_DIR=$("$BACKUP_DIRECTORY" | base64 -d)
+DB_USER=$(aws kms decrypt --key-id $KMS_KEY_ID --ciphertext-blob fileb://<(echo "$ACALSA_ENC_DB_USER" | base64 -d) --output text --query Plaintext | base64 -d)
+DB_PASSWORD=$(aws kms decrypt --key-id $KMS_KEY_ID --ciphertext-blob fileb://<(echo "$ACALSA_ENC_DB_PASSWORD" | base64 -d) --output text --query Plaintext | base64 -d)
+DB_INSTANCE=$(aws kms decrypt --key-id $KMS_KEY_ID --ciphertext-blob fileb://<(echo "$ACALSA_ENC_DB_INSTANCE" | base64 -d) --output text --query Plaintext | base64 -d)
+DB_INSTANCE_IDENTIFIER="instance-$DATE_FORMAT"
+
+# Error codes
+ERROR_BACKUP=1
+ERROR_EMPTY_BACKUP=2
+ERROR_CREATE_TEST_DB=3
+ERROR_RESTORE_BACKUP=4
+ERROR_DB_COMPARISON=5
+SUCCESSFULL_CLONE=200
 
 # Verificación de las variables de entorno para las credenciales
 if [ -z "$DB_USER" ] || [ -z "$DB_PASSWORD" ]; then
@@ -21,16 +30,9 @@ if [ -z "$DB_USER" ] || [ -z "$DB_PASSWORD" ]; then
     exit 1
 fi
 
-DATE_FORMAT=$(date +"%Y%m%d_%H%M%S")
-BACKUP_FILE="$BACKUP_DIR/$DB_NAME-$DATE_FORMAT.sql"
-TEST_DB_NAME="test_$DB_NAME"
-
-# Códigos de error
-ERROR_BACKUP=1
-ERROR_EMPTY_BACKUP=2
-ERROR_CREATE_TEST_DB=3
-ERROR_RESTORE_BACKUP=4
-ERROR_DB_COMPARISON=5
+BACKUP_FILE="$(pwd)/$ACALSA_BACKUP_DIRECTORY/$ACALSA_DB_NAME-$DATE_FORMAT.sql"
+TEST_DB_NAME="test_acalsa_db"
+TEST_BACKUP_FILE="$(pwd)/$ACALSA_BACKUP_DIRECTORY/$TEST_DB_NAME-$DATE_FORMAT.sql"
 
 # Función para mostrar mensajes de error y salir con un código de error
 error_exit() {
@@ -39,26 +41,26 @@ error_exit() {
 }
 
 # Comando mysqldump para respaldo
-mysqldump -h RDS_ENDPOINT -u $DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_FILE || error_exit $ERROR_BACKUP
+mysqldump --no-tablespaces -u $DB_USER -p$DB_PASSWORD -h $DB_INSTANCE $ACALSA_DB_NAME | gzip > "$BACKUP_FILE.gz" || error_exit $ERROR_BACKUP
 
 # Verificación del éxito del comando mysqldump
 if [ $? -eq 0 ]; then
     # Verificar que el archivo de respaldo no esté vacío
-    if [ -s "$BACKUP_FILE" ]; then
-        # Crear una base de datos de prueba y restaurar el respaldo
-        mysql -u $DB_USER -p$DB_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $TEST_DB_NAME" || error_exit $ERROR_CREATE_TEST_DB
-        mysql -u $DB_USER -p$DB_PASSWORD $TEST_DB_NAME < $BACKUP_FILE || error_exit $ERROR_RESTORE_BACKUP
+    if [ -s "$BACKUP_FILE.gz" ]; then
 
-        # Comparar las bases de datos original y restaurada
-        mysqldump -h RDS_ENDPOINT -u $DB_USER -p$DB_PASSWORD $DB_NAME > /tmp/original_db.sql  
-        mysqldump -u $DB_USER -p$DB_PASSWORD $TEST_DB_NAME > /tmp/restored_db.sql
+        # 🐰 Decompress the backup file and change its name
+        gunzip -c "$BACKUP_FILE.gz" > "$TEST_BACKUP_FILE"
 
-        # Comparar archivos
-        if diff /tmp/original_db.sql /tmp/restored_db.sql >/dev/null; then
-            echo "Las bases de datos son idénticas. Prueba unitaria exitosa."
-        else
-            error_exit $ERROR_DB_COMPARISON
+        if [ $? -eq 0 ]; then
+            mysql -u dev -pdev $TEST_DB_NAME
+            # 🐿️ Restore the database
+            mysql -u dev -pdev $TEST_DB_NAME < $TEST_BACKUP_FILE
+
+            if [ $? -eq 0 ]; then
+                echo $SUCCESSFULL_CLONE
+            fi
         fi
+
     else
         error_exit $ERROR_EMPTY_BACKUP
     fi
